ac8aa963f48a2bd56e6aa859dc1af6e4b9955075
Kenneth Ekman <kenneth@ekmansoft.com>
2021-10-03 14:05:56 +0200

    Add name equivalence database again..

--- FamilyTreeCompareResults.cs
+++ FamilyTreeCompareResults.cs
@@ -52,36 +52,119 @@ namespace FamilyTreeTools.CompareResults
       public string baseName;
       public IList<string> equivalentNames;
 
-      public NameEquivalences()
+      public NameEquivalences(string baseName)
       {
         equivalentNames = new List<string>();
+        this.baseName = baseName.ToLower();
       }
-      public string MinimizeName(string name)
+      public void AddEquivalent(string variant)
       {
+        equivalentNames.Add(variant.ToLower());
+      }
+      public bool IsEquivalent(string name)
+      {
+        if (name.ToLower() == baseName)
+        {
+          return true;
+        }
         foreach (string eq in equivalentNames)
         {
-          name = name.Replace(eq, baseName);
+          if (eq == name.ToLower())
+          {
+            return true;
+          }
         }
-        return name;
+        return false;
       }
     }
 
     public class NameEquivalenceDb
     {
-      public IList<NameEquivalences> equivalentNames;
+      public IDictionary<string,NameEquivalences> equivalentNames;
 
       public NameEquivalenceDb()
       {
-        equivalentNames = new List<NameEquivalences>();
+        equivalentNames = new Dictionary<string,NameEquivalences>();
+      }
+      public void AddEquivalent(string baseName, string equivalence)
+      {
+        if (!equivalentNames.ContainsKey(baseName))
+        {
+          equivalentNames.Add(baseName, new NameEquivalences(baseName));
+        }
+        NameEquivalences variant;
+        if (equivalentNames.TryGetValue(baseName, out variant))
+        { 
+          variant.AddEquivalent(equivalence);
+          equivalentNames.Add(baseName, variant);
+        }
       }
 
-      public string MinimizeName(string name)
+      public string SimplifyName(string fullName)
       {
-        foreach (NameEquivalences eq in equivalentNames)
+        IList<string> subNames = fullName.Split(" ");
+        IList<string> resultNames = new List<string>();
+
+        foreach (string name in subNames)
         {
-          name = eq.MinimizeName(name);
+          string lName = name.ToLower();
+          foreach (string eq in equivalentNames.Keys)
+          {
+            if (lName == eq)
+            {
+              //return name.ToLower();
+              resultNames.Add(lName);
+            }
+          }
+          foreach (NameEquivalences eq in equivalentNames.Values)
+          {
+            if (eq.IsEquivalent(name))
+            {
+              resultNames.Add(eq.baseName);
+            }
+          }
         }
-        return name;
+        trace.TraceInformation("Name [" + fullName + "] simplifies to [" + string.Join(" ", resultNames) + "]");
+        return string.Join(" ", resultNames);
+      }
+      public void LoadDefault()
+      {
+        AddEquivalent("kristina", "christina");
+        AddEquivalent("kristina", "stina");
+        AddEquivalent("kristina", "cristina");
+
+        AddEquivalent("katarina", "catarina");
+        AddEquivalent("katarina", "catharina");
+
+        AddEquivalent("karl", "carl");
+        AddEquivalent("karl", "kalle");
+
+        AddEquivalent("klara", "clara");
+
+        AddEquivalent("kristian", "christian");
+        AddEquivalent("kristian", "cristian");
+
+        AddEquivalent("oskar", "oscar");
+
+        AddEquivalent("valdemar", "waldemar");
+
+        AddEquivalent("manfrid", "manfred");
+
+        AddEquivalent("eriksson", "ericsson");
+        AddEquivalent("eriksson", "eriksdotter");
+
+        AddEquivalent("nilsson", "nilsdotter");
+
+        AddEquivalent("per", "pehr");
+        AddEquivalent("per", "pähr");
+
+        AddEquivalent("persson", "pehrsson");
+        AddEquivalent("persson", "pährsson");
+        AddEquivalent("persson", "pärsson");
+        AddEquivalent("persson", "pehrson");
+        AddEquivalent("persson", "persdotter");
+        AddEquivalent("persson", "pehrsdotter");
+
       }
     }
 
@@ -105,41 +188,43 @@ namespace FamilyTreeTools.CompareResults
     {
       public DefaultNameEquivalenceDb()
       {
-        NameEquivalences stina = new NameEquivalences();
+        //NameEquivalences stina = new NameEquivalences();
 
-        stina.baseName = "kristina";
-        stina.equivalentNames.Add("stina");
-        stina.equivalentNames.Add("christina");
-        stina.equivalentNames.Add("cristina");
-        equivalentNames.Add(stina);
+        //stina.baseName = "kristina";
+        //stina.equivalentNames.Add("stina");
+        //stina.equivalentNames.Add("christina");
+        //stina.equivalentNames.Add("cristina");
+        //equivalentNames.Add(stina);
 
-        NameEquivalences karl = new NameEquivalences();
-        karl.baseName = "karl";
-        karl.equivalentNames.Add("carl");
-        karl.equivalentNames.Add("kalle");
-        equivalentNames.Add(karl);
+        //NameEquivalences karl = new NameEquivalences();
+        //karl.baseName = "karl";
+        //karl.equivalentNames.Add("carl");
+        //karl.equivalentNames.Add("kalle");
+        //equivalentNames.Add(karl);
 
-        NameEquivalences kristian = new NameEquivalences();
-        kristian.baseName = "kristian";
-        kristian.equivalentNames.Add("christian");
-        kristian.equivalentNames.Add("cristian");
-        equivalentNames.Add(kristian);
+        //NameEquivalences kristian = new NameEquivalences();
+        //kristian.baseName = "kristian";
+        //kristian.equivalentNames.Add("christian");
+        //kristian.equivalentNames.Add("cristian");
+        //equivalentNames.Add(kristian);
 
-        NameEquivalences eriksson = new NameEquivalences();
-        eriksson.baseName = "eriksson";
-        eriksson.equivalentNames.Add("ericsson");
-        equivalentNames.Add(eriksson);
+        //NameEquivalences eriksson = new NameEquivalences();
+        //eriksson.baseName = "eriksson";
+        //eriksson.equivalentNames.Add("ericsson");
+        //equivalentNames.Add(eriksson);
 
-        NameEquivalences per = new NameEquivalences();
-        per.baseName = "per";
-        per.equivalentNames.Add("pehr");
-        equivalentNames.Add(per);
+        //NameEquivalences per = new NameEquivalences();
+        //per.baseName = "per";
+        //per.equivalentNames.Add("pehr");
+        //equivalentNames.Add(per);
 
-        NameEquivalences persson = new NameEquivalences();
-        persson.baseName = "persson";
-        persson.equivalentNames.Add("pehrsson");
-        equivalentNames.Add(persson);
+        //NameEquivalences persson = new NameEquivalences();
+        //persson.baseName = "persson";
+        //persson.equivalentNames.Add("pehrsson");
+        //equivalentNames.Add(persson);
+        //equivalentNames.AddEquivalent("kristina", "christina");
       }
+
 
     }
 
@@ -151,10 +236,10 @@ namespace FamilyTreeTools.CompareResults
     static bool IsNamesEqual(string name1, string name2, NameEquivalenceDb nameEqDb)
     {
       name1 = NormalizeName(name1);
-      //name1 = nameEqDb.MinimizeName(name1);
+      name1 = nameEqDb.SimplifyName(name1);
 
       name2 = NormalizeName(name2);
-      //name2 = nameEqDb.MinimizeName(name2);
+      name2 = nameEqDb.SimplifyName(name2);
 
       return name1 == name2;
     }
@@ -398,6 +483,7 @@ namespace FamilyTreeTools.CompareResults
       int cnt1 = 0;
 
       NameEquivalenceDb equivDb = new DefaultNameEquivalenceDb();
+      equivDb.LoadDefault();
 
       iterator1 = familyTree1.SearchPerson(null, reporter);
 
